3a42c267d66431ad0ea7e4c9b7f5778ed886f253
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-01 12:55:22 -0400

    adds index route

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 const pokemon = require("./pokedex/models/pokemon.js");
 
 app.use(express.urlencoded({extended : false}));
 app.use(methodOverride("_method"));
+
+app.get("/pokedex", (req, res)=>{
+    res.render("index_views.ejs", {
+        pokemons : pokemon,
+    });
+});
+
+app.listen(3000);
