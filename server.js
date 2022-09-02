039ed0b17acb6b4a25c934a5c021cb1d875b067f
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-01 21:15:18 -0400

    adds functionality

--- server.js
+++ server.js
@@ -13,15 +13,61 @@ app.get("/pokemon", (req, res)=>{
 });
 
 app.get("/pokemon/new", (req, res)=>{
-    res.render("new.ejs")
+    res.render("new.ejs",{
+        pokemon: pokemon,
+    })
 });
 
 app.delete("/pokemon/:id", (req, res)=>{
     pokemon.splice(req.params.id, 1)
     res.redirect("/pokemon");
-})
+});
+
+app.get("/pokemon/:id/edit", (req, res) =>{
+    res.render("edit.ejs",
+    {
+        pokemon: pokemon[req.params.id],
+        index: req.params.id,
+    });
+});
+
+app.put("/pokemon:id", (req, res)=>{
+    pokemon[req.params.id] = req.body;
+    let type = req.body.type;
+    let typeArr = type.split(" , ");
+    let stats ={
+    hp: req.body.hp,
+    attack: req.body.attack,
+    defense: req.body.defense,
+};
+
+let newPoke ={
+    name: req.body.name,
+    img: req.body.img,
+    type: typeArr,
+    stats: stats
+};
+    pokemon.push(newPoke);
+    console.log(newPoke);
+    res.redirect("/pokemon");
+});
+
 app.post("/pokemon", (req, res)=>{
-    pokemon.push(req.body);
+let type = req.body.type;
+let typeArr = type.split(" , ");
+let stats ={
+    hp: req.body.hp,
+    attack: req.body.attack,
+    defense: req.body.defense,
+};
+
+let newPoke ={
+    name: req.body.name,
+    img: req.body.img,
+    type: typeArr,
+    stats: stats
+};
+    pokemon.push(newPoke);
     res.redirect("/pokemon");
 });
 
